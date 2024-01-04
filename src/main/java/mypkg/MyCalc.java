4a01645ed41c6a230de5a3bfc0f9c39bbb62a349
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-01-04 15:26:21 +0530

    diff function added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -5,10 +5,15 @@ public class MyCalc {
 	{
 		return a+b;
 	}
+	int diff(int a, int b)
+	{
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
-		System.out.println("Sum is"+calc.sum(20, 10));	
+		System.out.println("Sum is "+calc.sum(20, 10));	
+		System.out.println("Diff is "+calc.diff(20, 10));
 
 	}
 }
